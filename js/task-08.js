0c7e01670c29f4a91a319b36814fc074dc78782a
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-03-15 13:34:33 +0200

    окончательное решение 8 задачи

--- js/task-08.js
+++ js/task-08.js
@@ -1,35 +1,39 @@
 const renderButton = document.querySelector('[data-action="render"]');
 const destroyButton = document.querySelector('[data-action="destroy"]');
 const boxes = document.getElementById('boxes')
+const input = document.querySelector('#controls input')
 console.log(renderButton);
 console.log(destroyButton);
 console.log(boxes);
 
-renderButton.addEventListener('click', createBoxes);
+renderButton.addEventListener('click', function () {
+    createBoxes(input.value)
+});
 destroyButton.addEventListener('click', destroyBoxes);
 
 
 function createBoxes(amount) {
     
-    const div = document.createElement('div');
-    div.style.width = '30px';
-    div.style.height = '30px';
-    div.style.backgroundColor = 'red';
-    // for (let i = 0; i < amount; i += 1) {
-    
-    //     const width = '30px' + i * 10;
-    //     const height = '30px' + i * 10;
-    // }
 
+    for (let i = 1; i <= amount; i += 1) {
+        const div = document.createElement('div');
+        div.style.width = `${20 + i * 10}px`;
+        div.style.height = `${20 + i * 10}px`;
+        div.style.backgroundColor = `rgba( ${random()} , ${random()} , ${random()}`;
+        boxes.append(div);
+    }
 
-    boxes.appendChild(div);
-    console.log(boxes);
+ 
 }
 
 
 function destroyBoxes() {
     boxes.innerHTML = ''
 
+}
+
+function random() {
+  return Math.floor(Math.random() * 256);
 }
 
 
